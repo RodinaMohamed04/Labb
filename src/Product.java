a6a4332618064a220b388cfc8e4a1d5acf18913a
Lenovo <Lenovo@Ghadeer-PC>
2025-10-18 13:45:25 +0300

    product class initialize

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,60 @@
+//package lab4;
+
 public class Product {
+
+    private String productID;
+    private String productName;
+    private String manufacturerName;
+    private String supplierName;
+    private int quantity;
+    private float price;
+
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public int getQuantity() {
+
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        ////////law take 1 >>-1///law return 1>>+1///lw n==0>>sold out
+       
+        this.quantity = quantity;
+    }
+
+    public String lineRepresentation() {
+        return getProductID() + "," + getProductName() + "," + getManufacturerName() + "," + getSupplierName() + "," + getQuantity() + "," + getPrice();
+    }
+
+    public String getSearchKey() {
+        return getProductID();
+    }
+
 }
